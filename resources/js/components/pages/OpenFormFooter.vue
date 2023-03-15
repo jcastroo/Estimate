4901e05da6be754f4eb3f9c8e5c7b8f0bebc4277
João Castro <2201781@my.ipleiria.pt>
2023-03-15 10:33:40 +0000

    Update main

--- resources/js/components/pages/OpenFormFooter.vue
+++ resources/js/components/pages/OpenFormFooter.vue
@@ -14,18 +14,6 @@
           </span>
         </router-link>
       </div>
-      <ul class="flex justify-center mt-5 md:mt-1">
-          <li class="mr-10">
-            <router-link :to="{name:'privacy-policy'}" class="text-gray-600 dark:text-gray-400 transition-colors duration-300 hover:text-nt-blue">
-              Privacy Policy
-            </router-link>
-          </li>
-          <li class="list-disc pl-3">
-            <router-link :to="{name:'terms-conditions'}" class="text-gray-600 dark:text-gray-400 transition-colors duration-300 hover:text-nt-blue">
-              Terms & Conditions
-            </router-link>
-          </li>
-        </ul>
     </div>
   </div>
 </template>
