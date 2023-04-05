1331954cf4df289cc464a611146fa38204df6066
João Castro <2201781@my.ipleiria.pt>
2023-04-05 10:38:30 +0100

    update

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -1,20 +1,4 @@
-@php
-  $config = [
-      'appName' => config('app.name'),
-      'locale' => $locale = app()->getLocale(),
-      'locales' => config('app.locales'),
-      'githubAuth' => config('services.github.client_id'),
-      'notion' => [
-          'worker' => config('services.notion.worker'),
-      ],
-      'links' => config('links'),
-      'production' => App::isProduction(),
-      'hCaptchaSiteKey' => config('services.h_captcha.site_key'),
-      'google_analytics_code' => config('services.google_analytics_code'),
-      'amplitude_code' => config('services.amplitude_code'),
-      'crisp_website_id' => config('services.crisp_website_id'),
-  ];
-@endphp
+
 
 <!DOCTYPE html>
 
@@ -24,23 +8,10 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  @vite('resources/js/app.js')
 
-  <link rel="icon" href="{{asset('/img/logob.png')}}">
 
   <title>Setup Estimate</title>
 
-  @if($meta)
-    <title>{{$meta['title']}}</title>
-    <meta name='description' content='{{$meta['description']}}'>
-
-    <meta name='og:title' content='{{$meta['title']}}'>
-    <meta name='og:description' content='{{$meta['description']}}'>
-    <meta name='og:image' content='{{$meta['image']}}'>
-    <meta name='og:site_name' content='Setup Estimate'>
-
-
-  @endif
 
 
 </head>
