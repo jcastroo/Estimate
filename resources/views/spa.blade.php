5866529521d2e9a6039ed06e3e9490c9150203a0
João Castro <2201781@my.ipleiria.pt>
2023-04-05 10:58:20 +0100

    update

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -32,11 +32,7 @@
 
   @if($meta)
     <title>{{$meta['title']}}</title>
-    <meta name='description' content='{{$meta['description']}}'>
-
     <meta name='og:title' content='{{$meta['title']}}'>
-    <meta name='og:description' content='{{$meta['description']}}'>
-    <meta name='og:image' content='{{$meta['image']}}'>
     <meta name='og:site_name' content='Setup Estimate'>
 
 
